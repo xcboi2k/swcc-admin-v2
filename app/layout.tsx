258ed1014acf42ccf9a19caec80829f7191f80b7
agarcia_vashcorp <darkspikyshark@gmail.com>
2026-03-16 10:53:17 +0800

    fix: Update toasts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NextTopLoader from 'nextjs-toploader'
+import { ToastContainer } from 'react-toastify'
+
 import './globals.css'
 import AuthProvider from '@/context/AuthProvider'
 import { colors } from '@/constants/themes'
@@ -35,6 +37,13 @@ export default function RootLayout({
                     showAtBottom={false}
                 />
                 <AuthProvider>{children}</AuthProvider>
+                <ToastContainer
+                    position="bottom-right" // Position of the toast
+                    autoClose={5000} // Duration before toast closes
+                    hideProgressBar={true} // Show progress bar
+                    closeOnClick // Close on click
+                    pauseOnHover // Pause on hover
+                />
             </body>
         </html>
     )
--- stores/useEX20142015Store.ts
+++ stores/useEX20142015Store.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 import { v4 as uuidv4 } from 'uuid'
-import toast from 'react-hot-toast'
+import toast from 'react-toastify'
 
 import {
     addDoc,
