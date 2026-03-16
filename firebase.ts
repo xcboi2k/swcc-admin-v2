8ea04a283b7d9c1790004478bab3332f83132b0f
agarcia_vashcorp <darkspikyshark@gmail.com>
2026-03-16 11:27:15 +0800

    fix: Update configs and options

--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB2WVgabBBVAclhREmp12_i02JA9xutcqM",
-    authDomain: "action-figure-guide-app.firebaseapp.com",
-    projectId: "action-figure-guide-app",
-    storageBucket: "action-figure-guide-app.appspot.com",
-    messagingSenderId: "774997431008",
-    appId: "1:774997431008:web:3dab3d645d96dddc423685"
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+    appId: process.env.FIREBASE_APP_ID,
+}
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
